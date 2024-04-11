ac25cfd9c9c9669d0614b0c6ae95b9f1cc72be44
selin <selinkabak@protonmail.com>
2024-04-11 19:52:56 +0200

    add score field

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,10 @@
 object Main {
   def main(args: Array[String]): Unit = {
 
-    case class Player(name: String)
+    case class Player(name: String, score: Int)
 
-    val player1 = Player("Alice")
-    val player2 = Player("Bob")
+    val player1 = Player("Alice", 10)
+    val player2 = Player("Bob", 20)
 
     println(s"Player 1: ${player1.name}")
     println(s"Player 2: ${player2.name}")
