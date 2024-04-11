0a0fb81471f1e8b27ece3ff80c2bea85af6cbd43
selin <selinkabak@protonmail.com>
2024-04-11 19:54:29 +0200

    is this a merge conflict?

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,10 @@
 object Main {
   def main(args: Array[String]): Unit = {
 
-    case class Player(name: String)
+    case class Player(name: String, age: Int)
 
-    val player1 = Player("Selin")
-    val player2 = Player("Fabian")
+    val player1 = Player("Selin", 25)
+    val player2 = Player("Fabian", 30)
 
     println(s"Player 1: ${player1.name}")
     println(s"Player 2: ${player2.name}")
