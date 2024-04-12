a0c02021bd1ea947e9b4d9498d7f5a6b6d071d1d
FabiSahne <hfranz90@gmail.com>
2024-04-12 11:09:10 +0200

    implement tests

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,9 +1,17 @@
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+  test("test card implementation") {
+    val c1 = uno.Card(uno.cardColors.RED, uno.cardValues.ONE)
+    assertEquals(c1.color, uno.cardColors.RED)
+    assertEquals(c1.value, uno.cardValues.ONE)
+    val c2 = uno.Card(uno.cardColors.BLUE, uno.cardValues.ONE)
+    val c3 = uno.Card(uno.cardColors.RED, uno.cardValues.TWO)
+    val c4 = uno.Card(uno.cardColors.BLUE, uno.cardValues.TWO)
+    // test can play card
+    assert(uno.canPlayCard(c2, c1))
+    assert(uno.canPlayCard(c3, c1))
+    assert(!uno.canPlayCard(c4, c1))
+    assert(uno.canPlayCard(c4, c3))
   }
 }
