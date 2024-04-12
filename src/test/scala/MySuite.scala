d761a99eb67d93d893df4abfe82c629ed6b808db
FabiSahne <hfranz90@gmail.com>
2024-04-12 12:21:54 +0200

    changed tests to ScalaTest and made some simple tests

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,17 +1,27 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
-class MySuite extends munit.FunSuite {
-  test("test card implementation") {
-    val c1 = uno.Card(uno.cardColors.RED, uno.cardValues.ONE)
-    assertEquals(c1.color, uno.cardColors.RED)
-    assertEquals(c1.value, uno.cardValues.ONE)
-    val c2 = uno.Card(uno.cardColors.BLUE, uno.cardValues.ONE)
-    val c3 = uno.Card(uno.cardColors.RED, uno.cardValues.TWO)
-    val c4 = uno.Card(uno.cardColors.BLUE, uno.cardValues.TWO)
-    // test can play card
-    assert(c2.canBePlayedOn(c1))
-    assert(c3.canBePlayedOn(c3))
-    assert(!c4.canBePlayedOn(c1))
-    assert(c4.canBePlayedOn(c3))
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+class MySuite extends AnyWordSpec {
+
+  "Uno" should {
+    "have cards" in {
+      val c1 = uno.Card(uno.cardColors.RED, uno.cardValues.ONE)
+      c1.color shouldEqual uno.cardColors.RED
+      c1.value shouldEqual uno.cardValues.ONE
+      val c2 = uno.Card(uno.cardColors.BLUE, uno.cardValues.ONE)
+      val c3 = uno.Card(uno.cardColors.RED, uno.cardValues.TWO)
+      val c4 = uno.Card(uno.cardColors.BLUE, uno.cardValues.TWO)
+      assert(c2.canBePlayedOn(c1))
+      assert(c3.canBePlayedOn(c3))
+      assert(!c4.canBePlayedOn(c1))
+      assert(c4.canBePlayedOn(c3))
+    }
+    "have players" in {
+      val hand1: List[uno.Card] = List(uno.Card(uno.cardColors.RED, uno.cardValues.ONE), uno.Card(uno.cardColors.RED, uno.cardValues.TWO))
+      val p1 = uno.Player(0, hand1)
+      val topCard = uno.Card(uno.cardColors.RED, uno.cardValues.TWO)
+      assert(p1.canPlay(topCard))
+      val topCard2 = uno.Card(uno.cardColors.BLUE, uno.cardValues.THREE)
+      assert(!p1.canPlay(topCard2))
+    }
   }
 }
