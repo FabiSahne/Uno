ccad748c83ce707113a27f2684ffb06100f75644
selin <selinkabak@protonmail.com>
2024-04-18 20:43:52 +0200

    more tests :3

--- src/main/scala/uno/Main.scala
+++ src/main/scala/uno/Main.scala
@@ -1,8 +1,18 @@
 package uno
 
-import uno.cardColors._
-import uno.cardValues._
-
 @main def hello(): Unit =
-  val green_five = Card(GREEN, FIVE)
-  println(green_five)
+  // Create a deck of cards
+  val deck = new Deck()
+  deck.shuffle()
+
+  // Create players with initial hands
+  val player1 = Player(0, deck.draw(7))
+  val player2 = Player(0, deck.draw(7))
+
+  // Create a list of players
+  val players = List(player1, player2)
+
+  // Print the initial state of the game
+  println(s"Player 1's hand: ${player1.hand}")
+  println(s"Player 2's hand: ${player2.hand}")
+  println(s"Remaining cards in deck: ${deck.allCards.size}")
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,3 +1,4 @@
+package uno
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 class MySuite extends AnyWordSpec {
@@ -15,6 +16,7 @@ class MySuite extends AnyWordSpec {
       assert(!c4.canBePlayedOn(c1))
       assert(c4.canBePlayedOn(c3))
     }
+
     "have players" in {
       val hand1: List[uno.Card] = List(uno.Card(uno.cardColors.RED, uno.cardValues.ONE), uno.Card(uno.cardColors.RED, uno.cardValues.TWO))
       val p1 = uno.Player(0, hand1)
@@ -22,6 +24,34 @@ class MySuite extends AnyWordSpec {
       assert(p1.canPlay(topCard))
       val topCard2 = uno.Card(uno.cardColors.BLUE, uno.cardValues.THREE)
       assert(!p1.canPlay(topCard2))
+    }
+
+    "shuffle deck" in {
+      val deck = new Deck()
+      val originalDeck = deck.allCards
+      val shuffledDeck = scala.util.Random.shuffle(originalDeck)
+
+      shuffledDeck should not equal originalDeck
+    }
+
+    "allow Wild Draw Four to be played on any card" in {
+      val c1 = uno.Card(uno.cardColors.RED, uno.cardValues.WILD_DRAW_FOUR)
+      val c2 = uno.Card(uno.cardColors.BLUE, uno.cardValues.WILD_DRAW_FOUR)
+      val c3 = uno.Card(uno.cardColors.RED, uno.cardValues.ONE)
+      val c4 = uno.Card(uno.cardColors.BLUE, uno.cardValues.TWO)
+      assert(c1.canBePlayedOn(c2))
+      assert(c1.canBePlayedOn(c3))
+      assert(c1.canBePlayedOn(c4))
+    }
+
+    "allow Wild to be played on any card" in {
+      val c1 = uno.Card(uno.cardColors.RED, uno.cardValues.WILD)
+      val c2 = uno.Card(uno.cardColors.BLUE, uno.cardValues.WILD)
+      val c3 = uno.Card(uno.cardColors.RED, uno.cardValues.ONE)
+      val c4 = uno.Card(uno.cardColors.BLUE, uno.cardValues.TWO)
+      assert(c1.canBePlayedOn(c2))
+      assert(c1.canBePlayedOn(c3))
+      assert(c1.canBePlayedOn(c4))
     }
   }
 }
