fe7c70054f8b2d58e09ad50abdbbc4f029424cae
FabiSahne <hfranz90@gmail.com>
2024-05-03 08:27:43 +0200

    rename 'hello' to 'main'

--- src/main/scala/uno/Main.scala
+++ src/main/scala/uno/Main.scala
@@ -4,7 +4,7 @@ import uno.controller.GameController
 import uno.models.Round
 import uno.views.TUI
 
-@main def hello(): Unit = {
+@main def main(): Unit = {
   val round = Round()
   val controller = new GameController(round)
   val tui = new TUI(controller)
