37a270d301269bcdd0fbd26e4058752539daf352
FabiSahne <hfranz90@gmail.com>
2024-05-28 10:55:41 +0200

    Command und Memento pattern

--- src/main/scala/uno/controller/GameController.scala
+++ src/main/scala/uno/controller/GameController.scala
@@ -1,9 +1,12 @@
 package uno.controller
 
-import uno.models._
+import uno.patterns.command.*
+import uno.models.*
+import uno.patterns.memento.*
 import uno.util.*
 
 class GameController(var round: Round) extends Observable:
+  private val caretaker = new Caretaker
   def initGame(): Unit =
     notifyObservers(Event.Start)
 
@@ -17,11 +20,30 @@ class GameController(var round: Round) extends Observable:
       quitGame()
       return
     }
+    saveState()
     round = round.copy(
       players = newPlayers,
       topCard = card,
       currentPlayer = (round.currentPlayer + 1) % round.players.length
     )
+    card.value match {
+      case cardValues.DRAW_TWO =>
+        val command = new DrawTwoCommand
+        executeCommand(command)
+      case cardValues.REVERSE =>
+        val command = new ReverseCommand
+        executeCommand(command)
+      case cardValues.SKIP =>
+        val command = new SkipCommand
+        executeCommand(command)
+      case cardValues.WILD =>
+        val command = new WildCommand
+        executeCommand(command)
+      case cardValues.WILD_DRAW_FOUR =>
+        val command = new WildDrawFourCommand
+        executeCommand(command)
+      case _ => ()
+    }
     notifyObservers(Event.Play)
 
   def drawCard(): Unit =
@@ -31,5 +53,16 @@ class GameController(var round: Round) extends Observable:
       round.players(round.currentPlayer).hand.addCard(newCard)
     )
     val newPlayers = round.players.updated(round.currentPlayer, newPlayer)
+    saveState()
     round = round.copy(players = newPlayers)
     notifyObservers(Event.Draw)
+
+  private def executeCommand(command: Command): Unit = {
+    command.execute(this)
+  }
+
+  private def saveState(): Unit =
+    caretaker.addMemento(Memento(round))
+
+  private def restoreState(): Unit =
+    caretaker.getMemento.foreach(memento => round = memento.round)
