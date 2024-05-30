2e0b1baec342c71b507b749aee9c64bf6b5b45c6
selin <selinkabak@protonmail.com>
2024-05-30 19:00:08 +0200

    implement undo mechanism

--- src/main/scala/uno/patterns/command/Command.scala
+++ src/main/scala/uno/patterns/command/Command.scala
@@ -4,39 +4,66 @@ import uno.controller.GameController
 
 trait Command {
   def execute(gameController: GameController): Unit
+  def undo(gameController: GameController): Unit
+  def redo(gameController: GameController): Unit
 }
 
 case class SkipCommand() extends Command {
   override def execute(gameController: GameController): Unit = {
-    // Implement the effect of a Skip card
     println("Skip command executed")
+  }
+  override def undo(gameController: GameController): Unit = {
+      println("Skip command undone")
+  }
+  override def redo(gameController: GameController): Unit = {
+      println("Skip command redone")
   }
 }
 
 case class ReverseCommand() extends Command {
   override def execute(gameController: GameController): Unit = {
-    // Implement the effect of a Reverse card
     println("Reverse command executed")
+  }
+  override def undo(gameController: GameController): Unit = {
+      println("Reverse command undone")
+  }
+  override def redo(gameController: GameController): Unit = {
+    println("Reverse command redone")
   }
 }
 
 case class DrawTwoCommand() extends Command {
   override def execute(gameController: GameController): Unit = {
-    // Implement the effect of a Draw Two card
     println("Draw Two command executed")
+  }
+  override def undo(gameController: GameController): Unit = {
+      println("Draw Two command undone")
+  }
+  override def redo(gameController: GameController): Unit = {
+      println("Draw Two command redone")
   }
 }
 
 case class WildCommand() extends Command {
   override def execute(gameController: GameController): Unit = {
-    // Implement the effect of a Wild card
     println("Wild command executed")
+  }
+  override def undo(gameController: GameController): Unit = {
+      println("Wild command undone")
+  }
+  override def redo(gameController: GameController): Unit = {
+      println("Wild command redone")
   }
 }
 
 case class WildDrawFourCommand() extends Command {
   override def execute(gameController: GameController): Unit = {
-    // Implement the effect of a Wild Draw Four card
     println("Wild Draw Four command executed")
+  }
+  override def undo(gameController: GameController): Unit = {
+      println("Wild Draw Four command undone")
+  }
+  override def redo(gameController: GameController): Unit = {
+      println("Wild Draw Four command redone")
   }
 }
--- src/main/scala/uno/util/UndoManager.scala
+++ src/main/scala/uno/util/UndoManager.scala
@@ -1,5 +1,35 @@
 package uno.util
 
-class UndoManager {
+import uno.controller.GameController
+import uno.patterns.command.Command
 
+class UndoManager {
+  private var undoStack: List[Command] = List()
+  private var redoStack: List[Command] = List()
+
+  def execute(command: Command, gameController: GameController): Unit = {
+    command.execute(gameController)
+    undoStack = command :: undoStack
+    redoStack = List()
+  }
+
+  def undo(gameController: GameController): Unit = {
+    undoStack match {
+      case Nil => println("Nothing to undo")
+      case head :: tail =>
+        head.undo(gameController)
+        undoStack = tail
+        redoStack = head :: redoStack
+    }
+  }
+
+  def redo(gameController: GameController): Unit = {
+    redoStack match {
+      case Nil => println("Nothing to redo")
+      case head :: tail =>
+        head.redo(gameController)
+        redoStack = tail
+        undoStack = head :: undoStack
+    }
+  }
 }
